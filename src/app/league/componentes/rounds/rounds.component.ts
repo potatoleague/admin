d0aa7518c9f639cd79b102167e1f599c35ad1c57
aledguedes <alexandredguedes@gmail.com>
2022-12-03 19:01:20 -0300

    instal moment

--- src/app/league/componentes/rounds/rounds.component.ts
+++ src/app/league/componentes/rounds/rounds.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-rounds',
@@ -10,9 +11,14 @@ export class RoundsComponent implements OnInit {
   firstTurn: any[] = [];
   secondTurn: any[] = [];
 
+  k = 1;
+  last_date = new Date();
+
   constructor() { }
 
   ngOnInit(): void {
+    // let teste = moment(moment(this.last_date), "MM-DD-YYYY").add(7, 'days');
+    // console.log('teste', teste)
   }
 
   gerar() {
@@ -32,7 +38,7 @@ export class RoundsComponent implements OnInit {
     const time01 = time;
     // console.log('ORIGINAL', time01, time02);
     this.firstTurn = this.segundoTurno(time01, time02, lengthArray);
-    this.secondTurn = this.segundoTurno(time02, time01, lengthArray);
+    this.secondTurn = this.segundoTurno(time01, time02, lengthArray);
   }
 
   segundoTurno(timeA: any, timeB: any, lengthArray: any) {
@@ -40,35 +46,40 @@ export class RoundsComponent implements OnInit {
     let mandator: any;
     let visitor: any;
     let i = 0;
-    let k = 1;
 
     for (i = 0; i < lengthArray - 1; i++) {
-      let rodada = k,
-        dt_partida = new Date();
+      let id = this.k,
+        dt_partida = this.last_date,
+        status = true;
       round[i] = {
-        rodada,
+        id,
         dt_partida,
+        status,
         jogos: [],
       };
       for (let j = 0; j < lengthArray / 2; j++) {
-        mandator = timeA[j];
-        visitor = timeB[j];
+        if (id % 2 == 0) {
+          mandator = timeA[j];
+          visitor = timeB[j];
+        } else {
+          mandator = timeB[j];
+          visitor = timeA[j];
+        }
 
         round[i].jogos.push({
           mandator,
           visitor,
         });
+        // this.last_date = this.last_date.getTime + 7;
       }
-      k++;
+      this.k++;
       const back1 = timeB.splice(0, 1); // BA
       const back2 = timeA.splice(3, 1); //MG
       timeA.splice(1, 0, back1[0]);
       timeB.splice(3, 0, back2[0]);
-      console.log('time A', timeA);
-      console.log('time B', timeB);
     }
-    // console.log('RESULT SEGUNDO TURNO', round);
-    // this.secondTurn = round;
+    console.log('RESULT SEGUNDO TURNO', round);
+    this.secondTurn = round;
     return round;
   }
 
